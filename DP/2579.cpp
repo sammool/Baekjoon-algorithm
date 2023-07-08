34df60df0ac498589d97a69e33f9cfa24c8be883
박찬규 <105445576+sammool@users.noreply.github.com>
2023-07-08 04:56:35 +0000

    DP

---
+++ DP/2579.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int dp[301];
+int arr[301];
+
+int main()
+{
+    int stair;
+    cin >> stair;
+    for(int i=1; i<=stair; i++)
+        cin >> arr[i];
+    
+    dp[1]=arr[1];
+    dp[2]=arr[1]+arr[2];
+    dp[3]=max(arr[1]+arr[3], arr[2]+arr[3]);
+
+    for(int i=4; i<=stair; i++)
+    {
+        dp[i]=max(dp[i-2]+arr[i], dp[i-3]+arr[i-1]+arr[i]);    
+    }
+    cout<<dp[stair];
+}
+    
