6764b697beeaa07af481813af36d597eff3a9dac
박찬규 <105445576+sammool@users.noreply.github.com>
2023-07-14 13:32:05 +0000

    Coins

--- DP/3067.cpp
+++ DP/3067.cpp
@@ -6,6 +6,9 @@ int dp[10001];
 
 int main()
 {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
     int t_case;
     cin >> t_case;
     for(int q=0; q<t_case; q++)
@@ -19,11 +22,20 @@ int main()
 
         int goal;
         cin >> goal;
-
-        dp[0]=0;
-        for(int i=1; i<=goal; i++)
+        
+        fill(dp,dp+10001,0);
+        dp[0]=1;
+        
+        for(int j=0; j<n; j++)
         {
-            
+            for(int i=1; i<=goal; i++)
+            {
+                if(i-coin[j]<0)
+                    continue;
+                dp[i]+=dp[i-coin[j]];   
+            }
         }
+        cout << dp[goal] << '\n';
     }
+
 }
