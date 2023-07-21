f16f7798e0a6b2418b0328d43ca190fd33d07d30
박찬규 <105445576+sammool@users.noreply.github.com>
2023-07-21 07:38:40 +0000

    test

---
+++ Two_Pointer/2230-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+long A[100001];
+
+int main()
+{
+    int n,m;
+    cin >> n >> m;
+
+    for(int i=0; i<n; i++)
+        cin >> A[i];
+    sort(A,A+n);
+
+    long min=2000000001;
+
+    int st=0;
+    int en=0;
+    for(int st=0; st<n; st++)
+    {
+        while()
+    }
+}
