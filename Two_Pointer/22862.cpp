2a5d7e8113bc43b16df9a71b912f019d31d0d5eb
박찬규 <105445576+sammool@users.noreply.github.com>
2023-07-24 11:13:36 +0000

    test

---
+++ Two_Pointer/22862.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int arr[100001];
+
+int main()
+{
+    int n, delete_n;
+    cin >> n >> delete_n;
+
+    for(int i=0; i<n; i++)
+        cin >> arr[i];
+    
+}
