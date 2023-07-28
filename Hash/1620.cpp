db259b3b9311b229bedaa50d295a505d322c90bc
박찬규 <105445576+sammool@users.noreply.github.com>
2023-07-28 07:58:26 +0000

    나는야 포켓몬 마스터 이다솜

--- Hash/1620.cpp
+++ Hash/1620.cpp
@@ -4,23 +4,30 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
     unordered_map <string,int> book;
+    string s[100001];
 
     int n,quiz;
     cin >> n >> quiz;
-    for(int i=1; i<n; i++)
+    for(int i=1; i<=n; i++)
     {
         string pocket;
         cin >> pocket;
-        book[pocket]=i;
+        
+        s[i] = pocket;
+        book[pocket] = i;
     }
     for(int i=1; i<=quiz; i++)
     {
         string _s;
         cin >> _s;
         if(isdigit(_s[0]))
-            cout << 
-
-    }
+            cout << s[stoi(_s)] << '\n';
+        else    
+            cout << book[_s] << '\n';
+   }
     
 }
