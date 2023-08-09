92331a0898a4aca5544d22a13463ece6477190d3
박찬규 <105445576+sammool@users.noreply.github.com>
2023-08-09 09:44:14 +0000

    절댓값 힙

--- Priority_queue/11286.cpp
+++ Priority_queue/11286.cpp
@@ -1,13 +1,23 @@
-#include <iostream>
-#include <queue>
+#include <bits/stdc++.h>
 
 using namespace std;
+
+class compare{
+    public:
+        bool operator()(const int& a, const int& b)
+        {
+            if(abs(a) != abs(b))
+                return abs(a) > abs(b);  //내림차순
+            return a>b;
+            //절대값이 같을때 음수가 루트로 가게 한다
+        }
+};
 
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    priority_queue <int, vector<int>, cmp> pq;
+    priority_queue <int, vector<int>, compare> pq;
 
     int n;
     cin >> n;
@@ -15,6 +25,18 @@ int main()
     {
         int x;
         cin >> x;
+        if(x==0)
+        {
+            if(pq.empty())
+                cout << 0 << '\n';
+            else
+            {
+                cout<<pq.top()<<'\n';
+                pq.pop();
+            }
+        }
+        else
+            pq.push(x);
         
     }
 }
