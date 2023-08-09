963a80ca7f4506c984d8881536dc98423c502bf7
박찬규 <105445576+sammool@users.noreply.github.com>
2023-08-09 08:16:54 +0000

    test

---
+++ Priority_queue/11286.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    priority_queue <int, vector<int>, cmp> pq;
+
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++)
+    {
+        int x;
+        cin >> x;
+        
+    }
+}
