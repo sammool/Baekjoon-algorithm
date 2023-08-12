e2d75f363ce79fc7704bc38d9e4ff5b3a1d4a752
박찬규 <105445576+sammool@users.noreply.github.com>
2023-08-12 05:21:54 +0000

    최소 힙

--- Priority_queue/1927.cpp
+++ Priority_queue/1927.cpp
@@ -7,7 +7,7 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    priority_queue <int, vector<int>, less<int>> pq;
+    priority_queue <int, vector<int>, greater<int>> pq;
     int n;
     cin >> n;
 
