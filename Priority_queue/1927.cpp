4bcfd08444135271b3c4137b4d065cb413d83177
박찬규 <105445576+sammool@users.noreply.github.com>
2023-08-12 05:20:02 +0000

    최대 힙

---
+++ Priority_queue/1927.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    priority_queue <int, vector<int>, less<int>> pq;
+    int n;
+    cin >> n;
+
+    for(int i=0; i<n; i++)
+    {
+        int x;
+        cin >> x;
+
+        if(x == 0)
+        {
+            if(pq.empty())
+                cout << 0 << '\n';
+            else
+             {
+                cout << pq.top() << '\n';
+                pq.pop();
+             } 
+        }
+        else
+            pq.push(x);
+    }
+
+}
