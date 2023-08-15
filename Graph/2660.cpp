a7dc5b26ba7f3852080cf9d7415dc4909a82d408
박찬규 <105445576+sammool@users.noreply.github.com>
2023-08-15 09:36:33 +0000

    회장 뽑기

--- Graph/2660.cpp
+++ Graph/2660.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 vector <int> adj[51];
 int score[51];
-bool vis[51];
+int dist[51];
 
 int main()
 {
@@ -21,13 +21,45 @@ int main()
 
     for(int i=1; i<=n; i++)
     {
+        fill(dist,dist+51,-1);
+
         queue <int> q;
         q.push(i);
-
+        dist[i] = 0;
+        
         while(!q.empty())
         {
+            int cnt = 0;
+            int cur = q.front();
+            q.pop();
+
+            for(int nxt : adj[cur])
+            {
+                if(dist[nxt]> -1)
+                    continue;
             
+                q.push(nxt);
+                dist[nxt] = dist[cur] + 1;
+            }    
+            score[i] = *max_element(dist,dist+51);
         }
-        score[i] ++
+      
+    }
+    
+    int min = *min_element(score+1,score+n-1);
+    int cnt = 0;
+    vector <int> v;
+    for(int i=1; i<=n; i++)
+    {
+        if(score[i] == min)
+        {
+            cnt++;
+            v.push_back(i);
+        }
+    }
+    cout << min <<" " << cnt << '\n';
+    for(int i=0; i<v.size(); i++)
+    {
+        cout << v[i] << " ";
     }
 }
