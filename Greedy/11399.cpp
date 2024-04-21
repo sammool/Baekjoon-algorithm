c539a6a96a6f027004cf26fa85274013ceeb88d6
박찬규 <105445576+sammool@users.noreply.github.com>
2024-04-21 06:49:02 +0000

    ATM

--- Greedy/11399.cpp
+++ Greedy/11399.cpp
@@ -4,6 +4,27 @@ using namespace std;
 int person[1001];
 int INF = 1001;
 
+// int main(){
+//     int n;
+//     cin >> n;
+
+//     fill(person,person+1001, INF);
+
+//     for(int i=0; i<n; i++)
+//         cin >> person[i];
+//     sort(person,person+1001);
+
+//     int time = 0;
+//     int result = 0;
+//     for(int i=0; i<n; i++){
+//         time+=person[i];
+//         result+=time;
+//     }
+
+//     cout << result;
+
+// }
+
 int main(){
     int n;
     cin >> n;
@@ -14,11 +35,10 @@ int main(){
         cin >> person[i];
     sort(person,person+1001);
 
-    int time = 0;
+    
     int result = 0;
     for(int i=0; i<n; i++){
-        time+=person[i];
-        result+=time;
+        result+= (n-i) *person[i];
     }
 
     cout << result;
